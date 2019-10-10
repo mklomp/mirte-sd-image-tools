537837716f5a381e86205d149c65486e8b8c5360
Martin Klomp <m.klomp@tudelft.nl>
2019-10-10 19:04:43 +0000

    Minor fixes on initial commit

--- install_zoef.sh
+++ install_zoef.sh
@@ -4,12 +4,14 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
-sudo apt install ros-melodic-ros-base python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
+sudo apt upgrade -y
+sudo apt install -y ros-melodic-ros-base python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source /opt/ros/melodic/setup.bash
 
 # Install Zoef ROS package (TODO: create rosinstall/rosdep)
-sudo apt install ros-melodic-controller-manager ros-melodic-rosbridge-server ros-melodic-diff-drive-controller
-mkdir ~/zoef_ws/src
+sudo apt install -y ros-melodic-controller-manager ros-melodic-rosbridge-server ros-melodic-diff-drive-controller
+mkdir -p ~/zoef_ws/src
 cd ~/zoef_ws/src
 git clone https://gitlab.tudelft.nl/rcj_zoef/zoef_ros_package.git
 cd ..
@@ -19,10 +21,11 @@ echo "source /home/zoef/zoef_ws/devel/setup.bash" >> ~/.bashrc
 # TODO: add systemd service to start ros
 
 # Install Zoef Interface
-sudo apt install singularity-container
+sudo apt install -y singularity-container
 cd ~
 git clone https://gitlab.tudelft.nl/rcj_zoef/web_interface.git
+cd web_interface
 sed -i 's/From: ubuntu:bionic/From: arm32v7\/ubuntu:bionic/g' Singularity
-./run_singularty init
+./run_singularity.sh init
 
 # TODO: add systemd service to start interface
