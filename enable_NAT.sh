88216cd3153698537303939d293f497f1c3e7722
Martin Klomp <m.klomp@tudelft.nl>
2019-10-22 08:51:24 +0000

    Added NAT routing script

---
+++ enable_NAT.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#NOTE: this should be in the install script, but that generates an error
+
+sudo apt install -y iptables-persistent
+
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+
+sudo sh -c "iptables-save > /etc/iptables/rules.v4"
