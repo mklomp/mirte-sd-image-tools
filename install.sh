ef2547c394e5c8ccf86cd76550972d0601035bb3
MeesCode <mbrinkhuis@student.tudelft.nl>
2020-05-23 16:55:14 +0200

    remove operating system dependency

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo apt install singularity-container
+# this depends on the host system used
+# sudo apt install singularity-container
+
 sudo rm -rf virtual_armbian.simg
 sudo singularity build --sandbox virtual_armbian.simg virtual_armbian.def
