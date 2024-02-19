eeb3d01de1f221a8228acb036277708b6882bbe6
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-02-19 20:15:33 +0100

    try faster build, revert to both armbian-resize and pishrink

--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -4,7 +4,7 @@ imagefile=$1
 if $INSTALL_PROVISIONING; then
 	sudo ./add_partition_local/add_partition.sh $imagefile
 fi
-sudo ./pishrink.sh -s $imagefile || true
+sudo ./pishrink.sh $imagefile || true
 filename=$(basename $imagefile .img)
 newImageFile="build/${filename}_$(date +"%Y-%m-%d_%H_%M_%S").img"
 echo "copying to $newImageFile"
