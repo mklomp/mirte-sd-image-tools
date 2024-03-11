0c80a149ee563adf1c1b7c972d4c8be925e46c0b
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-03-11 11:23:20 +0100

    rm armbian list fix

--- mirte_main_install.sh
+++ mirte_main_install.sh
@@ -8,7 +8,7 @@ if [[ $type == "mirte_rpi4b" ]]; then
 	echo "nameserver 8.8.8.8" >/etc/resolv.conf || true
 fi
 
-sudo mv /etc/apt/sources.list.d/armbian.list /etc/apt/sources.list.d/armbian.list.disabled || true
+sudo rm /etc/apt/sources.list.d/armbian.list || true
 
 chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo # something with sudo otherwise complaining about "sudo: /usr/bin/sudo must be owned by uid 0 and have the setuid bit set"
 . /usr/local/src/mirte/settings.sh                        # load settings
