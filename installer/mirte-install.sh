b7ef1d0ccf9b1248abdf4252e623c7f6d9dfadc4
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-07-01 15:25:54 +0200

    Fix relative path installer scripts

--- installer/mirte-install.sh
+++ installer/mirte-install.sh
@@ -4,37 +4,38 @@ EMMC_DEV=/dev/mmcblk0
 IMAGE=/root/mirte_orangepi3b.img
 # This script is used to install the Mirte image onto the emmc.
 picotool load -f /root/Telemetrix4RpiPico.uf2
-./set-text.sh "Mirte image installation started"
+picotool reboot -f
+/root/set-text.sh "Mirte image installation started"
 # Check if the script is run as root
 if [ "$EUID" -ne 0 ]; then
 	echo "Please run as root"
-	./set-text.sh "Please run as root"
+	/root/set-text.sh "Please run as root"
 	exit
 fi
 
 # check image exists
 if [ ! -f $IMAGE ]; then
 	echo "$IMAGE not found"
-	./set-text.sh "$IMAGE not found"
+	/root/set-text.sh "$IMAGE not found"
 	exit
 fi
 
 # check if the emmc exists
 if [ ! -b $EMMC_DEV ]; then
 	echo "emmc not found"
-	./set-text.sh "emmc not found"
+	/root/set-text.sh "emmc not found"
 	exit
 fi
 
 # write the image to the emmc
 echo "Writing $IMAGE to $EMMC_DEV"
-./set-text.sh "Writing $IMAGE to $EMMC_DEV"
+/root/set-text.sh "Writing $IMAGE to $EMMC_DEV"
 
 # start while loop in background, store pid
 while true; do
 	sleep 1
 	NEWLINE=$'\n'
-	./set-text.sh "Writing $IMAGE to $EMMC_DEV${NEWLINE}$(progress)"
+	/root/set-text.sh "Writing $IMAGE to $EMMC_DEV${NEWLINE}$(progress)"
 
 done &
 PID=$!
@@ -45,24 +46,25 @@ cat $IMAGE >$EMMC_DEV
 sync
 # kill the while loop
 kill $PID
-./set-text.sh "Mirte image written to $EMMC_DEV"
+/root/set-text.sh "Mirte image written to $EMMC_DEV"
 sleep 10
-./set-text.sh "Verifying image"
+/root/set-text.sh "Verifying image"
 # check if the image was written successfully by using checksum
 echo "Verifying $IMAGE"
 head -c "$(stat -c %s $IMAGE)" $EMMC_DEV | md5sum -c $IMAGE.md5sum
 
 # check return code
 if [ $? -eq 0 ]; then
-	./set-text.sh "Mirte image installed successfully"
+	/root/set-text.sh "Mirte image installed successfully"
 	echo "Mirte image installed successfully"
 else
-	./set-text.sh "Mirte image installation failed"
+	/root/set-text.sh "Mirte image installation failed"
 	echo "Mirte image installation failed"
 fi
 sleep 10
-./set-text.sh "Installing u-boot to spi"
+/root/set-text.sh "Installing u-boot to spi"
 source /usr/lib/u-boot/platform_install.sh
 write_uboot_platform_mtd $DIR /dev/mtdblock0
+/root/set-text.sh "Done installing, shutting down!"
 
 shutdown
--- installer/set-text.sh
+++ installer/set-text.sh
@@ -2,9 +2,9 @@
 
 TEXT=$1
 # write text to /root/text.txt
-echo "$TEXT" >./text.txt
+echo "$TEXT" >/root/text.txt
 
 # start set-text.py if it is not running
 if ! pgrep -f set-text.py; then
-	python3 ./set-text.py &
+	python3 /root/set-text.py &
 fi
