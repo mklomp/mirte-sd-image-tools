701ad323b1ed457320035573a0b72ddef6a48bc0
SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>
2024-10-04 17:48:28 +0200

    Document settings and ROS2 web interface start

--- default.settings.sh
+++ default.settings.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 export INSTALL_DOCS=false
-export INSTALL_ROS=true
+export INSTALL_ROS=true # FIXME: Non-functional: can a useable mirte install be made without ROS?
 export INSTALL_ARDUINO=false
-export INSTALL_WEB=true # TODO: Currently broken
+export INSTALL_WEB=true
+# Python should also be active when Web is active, however this is not enforced (otherwise limited functionality)
+# TODO: Should this be enforced
 export INSTALL_PYTHON=true
-export INSTALL_JUPYTER=false
+export INSTALL_JUPYTER=false # FIXME: Non-functional: Not setup for ROS2
 export EXPIRE_PASSWD=true
 export INSTALL_NETWORK=true
 export INSTALL_PROVISIONING=false
 export INSTALL_VSCODE=true
-export INSTALL_PAM=true
+export INSTALL_PAM=true # FIXME: Non-functional: can a useable mirte install be made without?
 export MIRTE_TYPE="default"
 export EXTRA_SCRIPTS=(
 	# "testExtra.sh"
 	# "testExtra2.sh"
 )
 export PARALLEL=true
+
+# Functional Settings:
+#  - INSTALL_WEB
+#  - INSTALL_PYTHON
+#  - INSTALL_VSCODE
