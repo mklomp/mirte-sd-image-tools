bf9cfb5249183d2dec4f75f42acf66c63c083a49
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-01-08 12:29:22 +0100

    Add add_overlay_partition option for on-sd overlay

--- mirte_main_install.sh
+++ mirte_main_install.sh
@@ -59,9 +59,15 @@ if $EXPIRE_PASSWD; then passwd --expire mirte; fi
 if $INSTALL_NETWORK; then /usr/local/src/mirte/mirte-install-scripts/network_install.sh; fi
 # for script in $${EXTRA_SCRIPTS[@]}; do /usr/local/src/mirte/$script; done
 
+# to use overlayroot: create an sd or usb with an ext4 partition with the label mirte_root. Any changes on the system will be put on the sd/usb instead of the main root file system (emmc)
+echo 'overlayroot=device:dev=LABEL=mirte_root,timeout=2' | sudo tee -a /etc/overlayroot.conf # overlayroot
+
 # install provisioning (not yet on main branch)
 # if $INSTALL_PROVISIONING; then
 #     mkdir /mnt/mirte # create mount point and automount it
 #     echo 'UUID="9EE2-A262" /mnt/mirte/ vfat rw,relatime,uid=1000,gid=1000,errors=remount-ro 0 0' >>/etc/fstab;
-# systemctl disable armbian-resize-filesystem # this is done by pishrink
 # fi
+
+if $ADD_OVERLAY_PARTITION; then
+	systemctl disable armbian-resize-filesystem # this will be done better by our script
+fi
